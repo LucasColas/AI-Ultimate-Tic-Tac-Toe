bbedafffd1da9c6c9169296434feb14c76379ef8
MrRex <lucas.colas13@gmail.com>
2020-07-16 18:48:58 +0200

    update

--- UTT.py
+++ UTT.py
@@ -4,6 +4,14 @@ import sys
 import random
 
 pygame.font.init()
-Width, Height = 750,750
+Width, Height = 770,770
+Square = Width//3
+Small_Square = Square//3
 
 Win = pygame.display.set_mode((Width, Height))
+
+Cross_small = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "cross.png")), (Small_Square, Small_Square))
+Cross = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "cross.png")), (Square, Square))
+
+Circle_small = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "circle.png")), (Small_Square, Small_Square))
+Circle = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "circle.png")), (Square, Square))
