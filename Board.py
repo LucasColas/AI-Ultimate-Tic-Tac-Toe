0b38d739188dc40a6f486b1ff4d26f18115f7ff7
MrRex <lucas.colas13@gmail.com>
2020-07-16 19:25:22 +0200

    update

--- Board.py
+++ Board.py
@@ -9,8 +9,12 @@ class new_Board():
         return [[for x in range(3)] for y in range(3)]
 
     def draw_board(self, Win, Lines_color, Width):
-        for i in range(1,3):
+        Height = Width
+        for i in range(1,3): #Draw horizontal lines
             pygame.draw.line(Win, Lines_color, (0, Width*(i/3)), (Width, Width*(i/3)), 1)
+
+        for j in range(1,3):
+            pygame.draw.line(Win, Lines_color, (Width*(j/3), 0), (Width*(j/3), Height), 1)
 
     def test(self):
         print("test class new_Board")
