3cc6b7a54b2f58280f1316b8cb34726c7947e273
MrRex <lucas.colas13@gmail.com>
2020-07-18 13:56:13 +0200

    update

--- Board.py
+++ Board.py
@@ -11,16 +11,20 @@ class new_Board():
     def every_small_boards(self):
         return [[[0 for x in range(3)] for y in range(3)] for z in range(3)]
 
-    def draw_board(self, Win, Lines_color, Width):
+    def draw_board(self, Win, Lines_color, Width, Square):
         Height = Width
+        for x in range(1,9):
+            pygame.draw.line(Win, Lines_color, (0, Square),(Width, Square*x) ,1)
+
+
         for i in range(1,3): #Draw horizontal lines
             pygame.draw.line(Win, Lines_color, (0, Width*(i/3)), (Width, Width*(i/3)), 1)
 
         for j in range(1,3):
             pygame.draw.line(Win, Lines_color, (Width*(j/3), 0), (Width*(j/3), Height), 1)
 
-    def draw_small_boards(self,Win, Lines_color, Square):
-        pass
+
+
 
 
     def test(self):
