eadf93a66ec2190ad692580a1a8dd80995ceaa0b
MrRex <lucas.colas13@gmail.com>
2020-07-19 21:18:52 +0200

    update

--- Board.py
+++ Board.py
@@ -15,15 +15,26 @@ class new_Board():
         Height = Width
 
         #Small Boards
+        for move in range(0,3):
+            for ab in range(0,3):
+                for x in range(1,3): #Vertical lines
+                    pygame.draw.line(Win, Lines_color_2, (margin + Square*move, (x*Small_Square) + ab*Square), ((Square-margin) + Square*move,(x*Small_Square) + ab*Square), 1)
+
+                for bc in range(0,2):
+                    for y in range(3): #Horizontal lines
+                        pygame.draw.line(Win, Lines_color_2, (Small_Square + bc*Small_Square, margin + ab*Square + move*Square), (Small_Square + bc*Small_Square, (Square-margin) + ab*Square + move*Square), 1)
+
+        """
+
         for ab in range(0,3):
             for x in range(1,3):
                 pygame.draw.line(Win, Lines_color_2, (margin, (x*Small_Square) + ab*Square), (Square-margin,(x*Small_Square) + ab*Square), 1)
-                
+
             for bc in range(0,2):
                 for y in range(3):
                     pygame.draw.line(Win, Lines_color_2, (Small_Square + bc*Small_Square, margin + ab*Square), (Small_Square + bc*Small_Square, (Square-margin) + ab*Square), 1)
 
-
+        """
         #Big Board
         for i in range(1,3): #Draw horizontal lines
             pygame.draw.line(Win, Lines_color, (0, Square*i), (Width, Square*i), 2)
