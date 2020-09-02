571df2d553277bdf946b264836b08fe7f4e63ca6
LucasColas <lucas.colas13@gmail.com>
2020-09-02 14:26:14 +0200

    update

--- UTT.py
+++ UTT.py
@@ -37,7 +37,7 @@ def valid_locations(board,main_board,x,y):
         for col in row:
             for i in range(0,7,3):
                 for indx, row in enumerate(board):
-                    if board[y][x] == 0 and main_board[indx//3][i//3] == 0:
+                    if board[y][x] == 0 and main_board[indx//3][i//3] != 0:
                         print("Valid")
                         return True
 
