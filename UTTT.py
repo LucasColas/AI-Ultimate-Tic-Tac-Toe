7afb33bbedc7fe15fa17cdd1a46f1cb7162da707
LucasColas <lucas.colas13@gmail.com>
2020-09-03 08:06:49 +0200

    update

--- UTTT.py
+++ UTTT.py
@@ -38,8 +38,8 @@ def valid_locations(board,main_board,x,y):
             for i in range(0,7,3):
                 for indx, row in enumerate(board):
                     if board[y][x] == 0 and main_board[indx//3][i//3] == 0:
-                        #print("indx :", indx//3)
-                        #print("i :", i//3)
+                        print("indx :", indx//3)
+                        print("i :", i//3)
                         print("Valid")
                         return True
 
@@ -75,6 +75,8 @@ def check_game(board,main_board, player):
                     check.clear()
 
 def place_big_board(main_board,i, indx, player):
+    print("indx//3 : ", indx//3)
+    print("i//3 : ", i//3)
     main_board[indx//3][i//3] = player
 
 def reset(board, main_board, game_over):
