b1ce91990de4eb42ccd35a8d9552864f75e44355
LucasColas <lucas.colas13@gmail.com>
2020-09-18 19:06:03 +0200

    update

--- Check_game.py
+++ Check_game.py
@@ -72,8 +72,6 @@ def Check_diagonals(board, main_board, player):
 
                     else:
                         stock_indx.clear()
-                else:
-                    stock_indx.clear()
 
     for x in range(0,9,3):
         stock_nindx = []
@@ -81,17 +79,17 @@ def Check_diagonals(board, main_board, player):
             #print(x,y)
             for i in range(3):
                 stock_nindx.append(board[y-1][x+1])
-                if len(stock_nindx) >= 3:
-                    print(stock_nindx)
-                    if stock_nindx.count(player) == len(stock_nindx):
-                        print(player, "succeeds (negative diags)")
-                        place_big_board(main_board, y//3, x//3, player)
-                        stock_nindx.clear()
-                    else:
-                        stock_nindx.clear()
-
+                print("added a value to check a negative diagonal")
+                print(stock_nindx)
+            if len(stock_nindx) >= 3:
+                print(stock_nindx)
+                if stock_nindx.count(player) == len(stock_nindx):
+                    print(player, "succeeds (negative diags)")
+                    place_big_board(main_board, y//3, x//3, player)
+                    stock_nindx.clear()
                 else:
                     stock_nindx.clear()
+
 
 def Check_empty_cells(board):
     if len(empty_cells_small_boards(board)) == 0:
