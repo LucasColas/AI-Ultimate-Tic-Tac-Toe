4b96957486e2d0c470795bf55a011cc62802e7f8
LucasColas <lucas.colas13@gmail.com>
2020-09-19 21:21:32 +0200

    update

--- Check_game.py
+++ Check_game.py
@@ -91,7 +91,6 @@ def Check_diagonals(board, main_board, player):
                     else:
                         stock_nindx.clear()
 
-
 def Check_empty_cells(board):
     if len(empty_cells_small_boards(board)) == 0:
         print("No one wins")
