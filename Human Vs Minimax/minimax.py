2db5690269a9625778c709c8f203f899ea608750
LucasColas <lucas.colas13@gmail.com>
2020-09-24 18:26:02 +0200

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -1,6 +1,6 @@
 from Check_game import empty_cells_small_boards
 from Check_game import Check_Big_Board
 
-def minimax(node, depth, player, MaximizingPlayer):
+def minimax(position, depth, player, MaximizingPlayer):
     if depth == 0 or Check_Big_Board:
-        return evaluate,node
+        return evaluate,position
