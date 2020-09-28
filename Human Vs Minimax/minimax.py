d0b01f8afe951af8629c1d93ae3b14e672a5eff2
LucasColas <lucas.colas13@gmail.com>
2020-09-28 12:27:44 +0200

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -2,16 +2,7 @@ from Check_game import empty_cells_small_boards
 from Check_game import Check_Big_Board
 
 def terminal_node(board, big_board):
-    return empty_cells_small_boards(board) or Check_Big_Board(big_board, 1) or Check_Big_Board(big_board, -1)
-
-"""
-To do :
--calculate the score of a Board (each time there are a piece in the big board : +1 or -1) :
-    *
--get a new board for a move
--stock those boards
-
-"""
+    return len(empty_cells_small_boards(board)) == 0 or Check_Big_Board(big_board, 1) or Check_Big_Board(big_board, -1)
 
 def minimax(node, board, big_board, depth, player, MaximizingPlayer):
     if depth == 0 or terminal_node(board, big_board):
@@ -27,3 +18,6 @@ def minimax(node, board, big_board, depth, player, MaximizingPlayer):
 def evaluate(node):
     score = 0
     return score
+
+def get_moves():
+    pass
