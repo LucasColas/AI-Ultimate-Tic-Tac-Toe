b9a5f0cd6d8fb254dff6b36c357af6b3129a3240
LucasColas <lucas.colas13@gmail.com>
2020-10-01 19:20:10 +0200

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -59,6 +59,7 @@ def get_moves(board, big_board,player):
         new_board[y][x] = player
         moves.append(new_board)
         new_big_board = deepcopy(big_board)
+        print(new_board)
         check_game(new_board,new_big_board, player)
         big_boards.append(new_big_board)
 
