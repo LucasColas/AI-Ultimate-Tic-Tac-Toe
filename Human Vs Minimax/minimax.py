9265118e3973a9ba155555dc6f2a606dd56110b6
LucasColas <lucas.colas13@gmail.com>
2020-10-06 20:35:04 +0200

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -5,14 +5,14 @@ from math import inf as infinity
 def minimax(node, big_board, depth, player, alpha, beta,MaximizingPlayer):
     if depth <= 0 or terminal_node(node, big_board, player):
         return evaluate(node),node
-    depth -= 1
+
 
     if MaximizingPlayer:
         value = -infinity
         good_node = None
         for node in get_moves(node, big_board,player):
             #print("node", node)
-            evaluation = minimax(node, big_board, depth, -1,alpha, beta, False)[0]
+            evaluation = minimax(node, big_board, depth-1, -1,alpha, beta, False)[0]
             value = max(value, evaluation)
             print("value", value)
             if value == evaluation:
@@ -29,7 +29,7 @@ def minimax(node, big_board, depth, player, alpha, beta,MaximizingPlayer):
         good_node = None
         for node in get_moves(node,big_board,player):
             #print("node", node)
-            evaluation = minimax(node, big_board, depth, 1,alpha, beta, True)[0]
+            evaluation = minimax(node, big_board, depth-1, 1,alpha, beta, True)[0]
             value = min(value, evaluation)
             if value == evaluation:
                 good_node = node
