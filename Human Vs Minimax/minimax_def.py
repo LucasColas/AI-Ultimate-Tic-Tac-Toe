fdb685553dcc7d17b024436e3e2ff06fa1ddfe32
LucasColas <lucas.colas13@gmail.com>
2020-10-08 19:55:59 +0200

    update

--- Human Vs Minimax/minimax_def.py
+++ Human Vs Minimax/minimax_def.py
@@ -93,8 +93,8 @@ def get_moves(board, big_board,player):
     big_boards = []
     for empty_cells in empty_cells_small_boards(board):
         print("empty_cells", empty_cells)
-        new_board = copy.copy(board)
-        new_big_board = copy.copy(big_board)
+        new_board = board.copy()
+        new_big_board = big_board.copy()
         x,y = empty_cells[0], empty_cells[1]
         #print(x//3,y//3)
         #if [x//3,y//3] in empty_cells_big_board(big_board):
