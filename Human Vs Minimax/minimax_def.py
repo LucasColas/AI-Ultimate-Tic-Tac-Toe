c40ed8df8aaa3eaff482d07e3ffe904c2ceb0e7e
LucasColas <lucas.colas13@gmail.com>
2020-10-08 13:59:08 +0200

    update

--- Human Vs Minimax/minimax_def.py
+++ Human Vs Minimax/minimax_def.py
@@ -96,7 +96,7 @@ def get_moves(board, big_board,player):
         new_board = deepcopy(board)
         new_big_board = deepcopy(big_board)
         x,y = empty_cells[0], empty_cells[1]
-        print(x,y)
+        print(x//3,y//3)
         if [x//3,y//3] in empty_cells_big_board(big_board):
             new_board[y][x] = player
             check_game(new_board, new_big_board, player)
