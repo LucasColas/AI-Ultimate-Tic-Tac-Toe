03e6779c08bf362dc6ec093acc3536c95ac09658
LucasColas <lucas.colas13@gmail.com>
2020-10-10 15:59:40 +0200

    update

--- Human Vs Minimax/UTTT.py
+++ Human Vs Minimax/UTTT.py
@@ -102,7 +102,7 @@ def main(small_boards, main_board):
                         turn = AI
 
         if turn == AI and not game_over:
-            depth = 1
+            depth = 5
             alpha, beta = -infinity, +infinity
 
             value, board = minimax(small_boards, main_board, depth, AI, alpha, beta,True)
