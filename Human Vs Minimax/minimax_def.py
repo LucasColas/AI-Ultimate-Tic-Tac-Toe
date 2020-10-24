560a0e08345e188510c38bc7f2ff1bc4cde5486e
LucasColas <lucas.colas13@gmail.com>
2020-10-24 13:23:40 +0200

    update

--- Human Vs Minimax/minimax_def.py
+++ Human Vs Minimax/minimax_def.py
@@ -1,3 +1,4 @@
+
 from Check_game import empty_cells_small_boards, empty_cells_big_board
 from Check_game import check_game, place_big_board, Check_Big_Board, Check_empty_cells
 from check_game import set_locations
