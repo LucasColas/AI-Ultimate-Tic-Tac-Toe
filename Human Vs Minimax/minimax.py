d730fb107324e88e6533f5cf74019d672612044b
LucasColas <lucas.colas13@gmail.com>
2020-10-24 13:35:55 +0200

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -13,14 +13,30 @@ def random_piece(board, big_board, player):
 
 def minimax_algo(board,big_board, depth, player, Maximizing):
     if depth <= 0 or terminal_node(board, big_board, player):
-        return [evaluate(board, big_board, player), -1, -1]
+        return [evaluate(board, big_board, player), board]
 
 
     if Maximizing:
+        maxEvaluation = float('-inf')
+        best = None
         for move in Possibles_moves(board, main_board, player):
-            
+            evaluation = minimax(board, big_board, depth-1, -1, False)[0]
+            maxEvaluation = max(maxEvaluation, evaluation)
+            if maxEvaluation == evaluation:
+                best = move
 
+        return maxEvaluation, best
 
+    else:
+        minEvaluation = float('inf')
+        best = None
+        for move in Possibles_moves(board, main_board, player):
+            evaluation = minimax(board, big_board, depth-1, -1, False)[0]
+            minEvaluation = min(maxEvaluation, evaluation)
+            if minEvaluation == evaluation:
+                best = move
+
+        return minEvaluation, best
     """
     if player == 1: #AI
         best = [-infinity, -1, -1]
