3f6fb42bffa00fe2d8bc305003521a694db235c4
LucasColas <lucas.colas13@gmail.com>
2020-11-13 08:58:01 +0100

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -1,3 +1,15 @@
+import copy
 #Récupérer les coups possibles / états
 #Les évaluer. Prendre le meilleur dans le cas de max.
 #Prendre le moins bon dans le cas de min
+
+
+def all_moves(Board, player):
+    all_moves = []
+    for y, row in Board:
+        for x, cell in Board:
+            if cell == 0:
+                new_Board = copy.deepcopy(Board)
+                new_Board[y][x] = player
+                
+    return all_moves
