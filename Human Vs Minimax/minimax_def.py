666023f238c0c0cf1f60664147df44b527279422
LucasColas <lucas.colas13@gmail.com>
2020-11-14 19:49:24 +0100

    update

--- Human Vs Minimax/minimax_def.py
+++ Human Vs Minimax/minimax_def.py
@@ -3,3 +3,16 @@ from Check_game import check_game, place_big_board, Check_Big_Board, Check_empty
 from Check_game import set_locations
 import copy
 #from math import inf as infinity
+Board = new_Board()
+Boards = Board.every_small_boards()
+def all_moves(Board, player):
+    all_moves = []
+    for y, row in Board:
+        for x, cell in Board:
+            if cell == 0:
+                new_Board = copy.deepcopy(Board)
+                new_Board[y][x] = player
+
+    return all_moves
+
+all_moves(Board, 1)
