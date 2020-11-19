45449839dd28a6f1060991b0286e555682603564
LucasColas <lucas.colas13@gmail.com>
2020-11-19 20:28:18 +0100

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -1,11 +1,10 @@
+from minimax_def import all_moves,  evaluate, terminal_state
 import copy
 
 #Récupérer les coups possibles / états
 #Les évaluer. Prendre le meilleur dans le cas de max.
 #Prendre le moins bon dans le cas de min
 
-def Minimax(board, depth, player, MaximizingPlayer):
-    if depth == 0 or terminal_state():
-        return evaluate(board, big_board, player)
-
-    
+def Minimax(Board, depth, player, MaximizingPlayer):
+    if depth == 0 or terminal_state(Board, player):
+        return evaluate(Board, player)
