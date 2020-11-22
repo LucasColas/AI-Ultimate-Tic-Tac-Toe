dd887baa91c9e003d6bf4c367e05d674fb7f1d9d
LucasColas <lucas.colas13@gmail.com>
2020-11-22 21:03:36 +0100

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -20,7 +20,7 @@ def Minimax(Board, depth, player, MaximizingPlayer):
         score = infinity
         Good_B = None
         for Board_ in all_moves(Board, player):
-            value = evaluate(Board_, depth-1, -player, False)[0]
+            value = evaluate(Board_, depth-1, -player, True)[0]
             score = min(value, score)
             if value == score:
                 Good_B = Board_
