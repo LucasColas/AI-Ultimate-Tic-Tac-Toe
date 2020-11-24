8261c945c39a7d8536ea955f4fd250b4d0aa4e89
LucasColas <lucas.colas13@gmail.com>
2020-11-24 21:22:07 +0100

    update

--- Human Vs Minimax/minimax_def.py
+++ Human Vs Minimax/minimax_def.py
@@ -17,7 +17,7 @@ def all_moves(Board, main_board, player):
                 new_Board = copy.deepcopy(Board)
                 new_main_board = copy.deepcopy(main_board)
                 if set_locations(new_Board, main_board, x,y, player):
-
+                    all_moves.append(new_Board)
 
     return all_moves
 
