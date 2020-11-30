0b2c50f8d9536c09ebd65761f4d815c2c75f8ac6
LucasColas <lucas.colas13@gmail.com>
2020-11-30 06:44:53 +0100

    update

--- Human Vs Minimax/minimax.py
+++ Human Vs Minimax/minimax.py
@@ -6,9 +6,9 @@ def Minimax(Board, main_board, depth, player, MaximizingPlayer):
     if depth == 0 or terminal_state(Board, player):
         if terminal_state(Board, player):
             if terminal_state(Board, 1):
-                 return (1000000,Board)
+                 return (10,Board)
             if terminal_state(Board,-1):
-                return (-1000000,Board)
+                return (-10,Board)
             else:
                 return (0, Board)
         else:
@@ -32,8 +32,8 @@ def Minimax(Board, main_board, depth, player, MaximizingPlayer):
         Good_B = None
         for Board_ in all_moves(Board, main_board, player):
             value = Minimax(Board_, main_board, depth-1, -player, True)[0]
-            print("value", value)
-            print("score", score)
+            #print("value", value)
+            #print("score", score)
             score = min(value, score)
             if value == score:
                 Good_B = Board_
