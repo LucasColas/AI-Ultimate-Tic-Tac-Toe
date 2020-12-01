862993bfa156c145780f388b8be93475be16fcb2
LucasColas <lucas.colas13@gmail.com>
2020-12-01 18:50:12 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -21,7 +21,7 @@ def Minimax(Board, main_board, depth, player, alpha, beta, MaximizingPlayer):
         Good_B = None
         for Board_ in all_moves(Board, main_board, player):
             print("Board : ", Board_)
-            value = Minimax(Board_, main_board, depth-1, -player, False)[0]
+            value = Minimax(Board_, main_board, depth-1, -player, alpha, beta, False)[0]
             score = max(value, score)
             if value == score:
                 Good_B = Board_
@@ -34,7 +34,7 @@ def Minimax(Board, main_board, depth, player, alpha, beta, MaximizingPlayer):
         score = float('inf')
         Good_B = None
         for Board_ in all_moves(Board, main_board, player):
-            value = Minimax(Board_, main_board, depth-1, -player, True)[0]
+            value = Minimax(Board_, main_board, depth-1, -player, alpha, beta, True)[0]
             #print("value", value)
             #print("score", score)
             score = min(value, score)
