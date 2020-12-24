72e32b7b600e6ec89546b9ca3b2b936b02c729fa
LucasColas <lucas.colas13@gmail.com>
2020-12-24 16:18:14 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -2,6 +2,8 @@ from minimax_def import all_moves,  evaluate, terminal_state
 import copy
 from math import inf as infinity
 
+#Evaluate each box
+
 def Minimax(Board, main_board, depth, player, alpha, beta, MaximizingPlayer):
     if depth == 0 or terminal_state(Board, player):
             if terminal_state(Board, player):
