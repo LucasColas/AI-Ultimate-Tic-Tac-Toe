2a907053f4caea3d53debd3e38760fdffbe84c14
LucasColas <lucas.colas13@gmail.com>
2020-12-27 19:35:22 +0100

    get small box

--- Human Vs Minimax (Alpha Beta Pruning)/minimax_assets.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax_assets.py
@@ -1,10 +1,13 @@
 def get_small_box(Board, player):
-    for i in range(3):
+    for i in range(0,9,3):
         small_box = []
         for j in range(0,9,3):
             for h in range(3):
                 for k in range(3):
-                    small_box.append(Board[j+h][j+k])
+                    small_box.append(Board[h+i][j+k])
+            #print(small_box)
+            small_box.clear()
+
 
 
 def evaluate_small_box(Board, player):
