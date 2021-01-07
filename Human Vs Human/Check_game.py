9bafac693626aa40fd1915342f1a04f56a461daa
LucasColas <lucas.colas13@gmail.com>
2021-01-07 06:45:05 +0100

    update

--- Human Vs Human/Check_game.py
+++ Human Vs Human/Check_game.py
@@ -51,7 +51,18 @@ def get_next_box(x,y):
                 possible_moves = []
                 for k in range(3,6):
                     for h in range(6,9):
-                possible_moves.append([k,h])
+                        possible_moves.append([k,h])
+
+                return possible_moves
+
+    for i in range(2,9,3):
+        for j in range(0,9,3):
+            if (x,y) == (i,j):
+                possible_moves = []
+                for k in range(6,9):
+                    for h in range(3):
+
+
 
 
 
