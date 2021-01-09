5c0ff5330bb21aa349c5cf4326e544b3cee32dd2
LucasColas <lucas.colas13@gmail.com>
2021-01-09 07:11:03 +0100

    update

--- Human Vs Human/UTTT.py
+++ Human Vs Human/UTTT.py
@@ -11,7 +11,7 @@ from Frontend import draw_board
 from Frontend import draw_big_pieces
 
 from Check_game import Check_horizontally, Check_vertically, Check_diagonals, Check_Big_Board, empty_cells_big_board, Check_empty_cells
-from Check_game import get_possible_moves
+from Check_game import get_possible_moves, is_empty_box
 
 pygame.font.init()
 Width, Height = 810,810
