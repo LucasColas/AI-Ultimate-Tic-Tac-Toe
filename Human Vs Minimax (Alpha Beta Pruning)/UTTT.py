d3f20e79ade07c296eb503a2288bc8c7c2b381b4
LucasColas <lucas.colas13@gmail.com>
2021-01-15 06:27:24 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/UTTT.py
+++ Human Vs Minimax (Alpha Beta Pruning)/UTTT.py
@@ -13,7 +13,8 @@ from Frontend import draw_big_pieces
 
 from Check_game import Check_Big_Board, empty_cells_big_board, Check_empty_cells, check_game, empty_cells_small_boards
 from Check_game import Check_empty_cells, set_locations, valid_locations
-from Check_game import check_game 
+from Check_game import check_game
+
 from minimax import Minimax
 
 
