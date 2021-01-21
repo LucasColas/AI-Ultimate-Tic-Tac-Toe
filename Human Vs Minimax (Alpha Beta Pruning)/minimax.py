ee32271dd74b7b1d772a0865440b553432695577
LucasColas <lucas.colas13@gmail.com>
2021-01-21 06:55:25 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -7,6 +7,8 @@ def minimax(Board, Main_board,Depth, Box, Player,MaximizingPlayer):
         pass
 
 def get_all_moves(Board, Main_board, Box, Player):
-    new_Board = copy.deecopy(Board)
-    new_Main_board = copy.deecopy(Board)
-    if set_locations()
+    for x,y in Box:
+        new_Board = copy.deecopy(Board)
+        new_Main_board = copy.deecopy(Board)
+        if set_locations(new_Board, new_Main_board,x,y, player,Box):
+            pass
