de47486fc612555228c140750e056d52cf7e5334
LucasColas <lucas.colas13@gmail.com>
2021-01-21 06:46:42 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -1,4 +1,8 @@
 
 
-def minimax( Box, Player,MaximizingPlayer):
-    pass
+def minimax(Board, Main_board,Depth, Box, Player,MaximizingPlayer):
+    if Depth == 0:
+        pass
+
+def get_all_moves(Board, Main_board, Box, Player):
+    
