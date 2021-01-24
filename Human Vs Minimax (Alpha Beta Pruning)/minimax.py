bb8ccdd1221934f7867f4c2993e601ff7fe44427
LucasColas <lucas.colas13@gmail.com>
2021-01-24 07:04:23 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -3,16 +3,6 @@ from Check_game import set_locations
 from Check_game import empty_cells_small_boards, empty_cells_big_board, Check_Big_Board
 import copy
 
-def is_terminal(Board, Main_board,Player):
-    if len(empty_cells_small_boards(Board)) == 0:
-        return True
-
-    if len(empty_cells_big_board(Main_board)) == 0:
-        return True
-
-    if Check_Big_Board(Main_board, Pllayer):
-        return True
-
 def minimax(Board, Main_board,Depth, Box, Player,MaximizingPlayer):
     if Depth == 0 or is_terminal(Board, Main_board, Player) :
         return Board, evaluate() #Function to define
@@ -52,3 +42,14 @@ def get_all_moves(Board, Main_board, Box, Player):
             all_Big_Boards.append(new_Main_board)
             all_Boxes.append(Good_Box)
     return all_Boards,all_Big_Boards, all_Boxes
+
+
+def is_terminal(Board, Main_board,Player):
+    if len(empty_cells_small_boards(Board)) == 0:
+        return True
+
+    if len(empty_cells_big_board(Main_board)) == 0:
+        return True
+
+    if Check_Big_Board(Main_board, Pllayer):
+        return True
