c14f26d1a725049fa9f13e40ec7610d89ca9de5f
LucasColas <lucas.colas13@gmail.com>
2021-01-24 06:30:09 +0100

    update

--- Human Vs Minimax (Alpha Beta Pruning)/minimax.py
+++ Human Vs Minimax (Alpha Beta Pruning)/minimax.py
@@ -18,8 +18,10 @@ def minimax(Board, Main_board,Depth, Box, Player,MaximizingPlayer):
         return Board, evaluate() #Function to define
 
     if MaximizingPlayer:
+        max = float('-inf')
+        Best_Board = None
         for Board_, Box_ in zip(get_all_moves(Board,Main_board, Box, Player)[0], get_all_moves(Board,Main_board, Box, Player)[1]):
-            pass
+            value = minimax()
 
     else:
         for Board_, Box_ in zip(get_all_moves(Board,Main_board, Box, Player)[0], get_all_moves(Board,Main_board, Box, Player)[1]):
@@ -36,5 +38,5 @@ def get_all_moves(Board, Main_board, Box, Player):
             Box = get_possible_moves(new_Board,x,y)
             Good_Box = Validate_box(new_Board, new_Main_board,Box,x,y)
             all_Boards.append(new_Board)
-            all_Boxes.append()
+            all_Boxes.append(Good_Box)
     return all_Boards, all_Boxes
